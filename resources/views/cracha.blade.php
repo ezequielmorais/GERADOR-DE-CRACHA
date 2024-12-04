38ff8d10a37056d35fc791b7e7aa7a3942a68240
ezequielmorais <ezequiel.morais@sistemafibra.org.br>
2024-12-04 11:00:14 -0300

    layout do cracha pdf

--- resources/views/cracha.blade.php
+++ resources/views/cracha.blade.php
@@ -3,156 +3,278 @@
 @section('Conteudo')
 
 <link rel="stylesheet" href="{{ asset('css/cracha.css') }}">
+<style>
+.drop-area {
+  width: 40px;
+  height: 80px;
+  border: 2px solid black;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #1c56a8;
+  font-size: 5px;
+  cursor: pointer;
+  text-align: center;
+  position: relative;
+
+}
+
+
+.drop-area,
+.image-preview {
+  margin-top: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  border: 2px solid #245ca8;
+  height: 120px;
+}
+
+.drop-area.dragover {
+  background-color: #f0f8f0;
+}
+
+.drop-area img {
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 8px;
+}
+
+.image-preview {
+  display: none;
+  margin-top: 70px;
+  text-align: center;
+
+}
+
+.form-container {
+  margin-top: 20px;
+  text-align: center;
+  display: none;
+}
+
+.centralizar-imagem {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 226px;
+}
+
+.card-container {
+  width: 360px;
+  margin: 20px auto;
+  border: 1px solid #ddd;
+  border-radius: 0px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  background-color: transparent;
+}
+
+.card-header {
+  padding: 0px;
+  background-color: transparent;
+  color: white;
+  font-size: 14px;
+  text-align: center;
+}
+
+.card-body {
+  padding: 20px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.card-body img {
+  max-width: 100%;
+  border-radius: 0px;
+}
+
+.usuario {
+  color: #1c56a8;
+  font-weight: 900;
+  font-family: "inter", Courier, monospace;
+  padding: 5px;
+  margin-bottom: 0px;
+  font-size: 15px;
+}
+
+.cargo {
+  font-family: "inter", Courier, monospace;
+  margin-top: 0px;
+  font-size: 12px;
+  font-weight: 700;
+  color: gray;
+}
+
+.usuario,
+.cargo {
+  margin-bottom: 0px;
+
+}
+
+.footer-img {
+  width: 226px;
+  height: 40px;
+  margin-top: 20px;
+}
+
+
+.crachas-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+}
+
+/* Nova classe para aumentar a margem do preview da imagem */
+.preview-image-second-cracha {
+  margin-top: 10px;
+
+}
+
+.footer-img1 {
+  width: 226px;
+  height: 40px;
+  margin-top: 57px;
+}
+
+.titulo {
+  font-size: 16px;
+  font-weight: bold;
+  color: #1c56a8;
+  margin-top: 30px;
+  margin-bottom: 2px;
+}
+
+.drop-area1 {
+  width: 90px;
+  height: 90px;
+  border: 2px solid #245ca8;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #245ca8;
+  font-size: 5px;
+  cursor: pointer;
+  text-align: center;
+  position: relative;
+  margin-top: 50px;
+  margin-bottom: -6px;
+}
+
+.card-body {
+  position: relative;
+}
+
+.matricula {
+  position: absolute;
+  top: 10px;
+  right: 90px;
+  font-size: 12px;
+  color: #ffffff;
+  background-color: transparent;
+  padding: 5px;
+  z-index: 10;
+  margin-top: 312px;
+  margin-bottom: 600px;
+}
+
+.footer-img {
+  position: relative;
+  z-index: 1;
+
+}
+
+.cod-matricula {
+  font-weight: 700;
+}
+</style>
 
 <body>
-    <form method="POST" action="{{ route('gerarPdf') }}" id="form-gerar-pdf">
-        @csrf
-        <input type="hidden" name="nome" value="{{ $nome }}">
-        <input type="hidden" name="cargo" value="{{ $cargo }}">
-        <input type="hidden" name="matricula" value="{{ $matricula }}">
-        <input type="hidden" name="casa" value="{{ $casa }}">
-        <input type="hidden" name="qrcode_url" value="{{ $qrcodeUrl }}">
-        <input type="hidden" name="image" value="{{ asset($imagePath ?? '') }}"> <!-- Caminho da imagem carregada -->
+  <form method="POST" action="{{ route('gerarPdf') }}" id="form-gerar-pdf">
+    @csrf
+    <input type="hidden" name="nome" value="{{ $nome }}">
+    <input type="hidden" name="cargo" value="{{ $cargo }}">
+    <input type="hidden" name="matricula" value="{{ $matricula }}">
+    <input type="hidden" name="casa" value="{{ $casa }}">
+    <input type="hidden" name="qrcode_url" value="{{ $qrcodeUrl }}">
+    <input type="hidden" name="image" value="{{ asset($imagePath ?? '') }}"> <!-- Caminho da imagem carregada -->
 
-        <button type="submit" class="btn btn-primary">Gerar PDF</button>
-    </form>
-    <div class="crachas-container">
-        <!-- Crachá 1 -->
-        <div class="card-container">
-            <div class="card-header">
+    <button type="submit" class="btn btn-primary">Gerar PDF</button>
+  </form>
+  <div class="crachas-container">
+    <!-- Crachá 1 -->
+    <div class="card-container">
+      <div class="card-header">
 
-            </div>
-            <div class="card-body" style="position: relative;">
-                <img src="img/cracha-frente-barra1.png" alt="Imagem do Cracha" class="centralizar-imagem">
+      </div>
+      <div class="card-body" style="position: relative;">
+        <img src="img/cracha-frente-barra1.png" alt="Imagem do Cracha" class="centralizar-imagem">
 
-                <div class="drop-area" id="drop-area">
-                    <!-- Exibe a imagem enviada, se houver -->
-                    @if($imagePath)
-                    <img id="preview-image" src="{{ asset($imagePath) }}" alt="Imagem carregada">
-                    @else
-                    <img id="preview-image" src="img/upload.png" alt="Pré-visualização">
-                    @endif
-                </div>
-
-                <div class="image-preview" id="image-preview">
-                    @if($imagePath)
-                    <img id="preview-image" src="{{ asset($imagePath) }}" alt="Imagem carregada">
-                    @else
-                    <img id="preview-image" src="img/upload.png" alt="Pré-visualização">
-                    @endif
-                </div>
-
-                <div class="form-container" id="form-container">
-                   
-                </div>
-                <p class="usuario">{{ $nome }}</p>
-                <p class="cargo">{{ $cargo }}</p>
-
-                <img class="footer-img" src="img/footer-img1.png" alt="footer">
-
-                <!-- Tag Matricula que ficará sobre a imagem -->
-                <p class="matricula">Matrícula <span class="cod-matricula">{{ $matricula }}</span></p>
-            </div>
-
-            <!-- cracha2 -->
-            <div class="card-header">
-                <!-- Aqui você pode adicionar título ou outros elementos se necessário -->
-            </div>
-            <div class="card-body">
-                <img src="img/rodape.png" alt="Imagem do Cracha" class="centralizar-imagem">
-                <p class="titulo">Presença</p>
-                <div class="drop-area1" id="drop-area">
-                    <img id="preview-image" src="{{ $qrcodeUrl }}" alt="QR Code">
-                </div>
-
-                <div class="image-preview" id="image-preview">
-                    <img id="preview-image" src="{{ $qrcodeUrl }}" alt="QR Code">
-                </div>
-
-                <div class="form-container" id="form-container">
-                    <form method="POST" action="/upload" enctype="multipart/form-data">
-                        @csrf
-                        <input type="hidden" name="image_data" id="image_data">
-                        <button type="submit" class="btn btn-primary">Enviar Imagem</button>
-                    </form>
-                </div>
-
-                <img class="footer-img1" src="img/cracha-rodape-costas.png" alt="footer">
-            </div>
+        <div class="drop-area" id="drop-area">
+          <!-- Exibe a imagem enviada, se houver -->
+          @if($imagePath)
+          <img id="preview-image" src="{{ asset($imagePath) }}" alt="Imagem carregada">
+          @else
+          <img id="preview-image" src="img/upload.png" alt="Pré-visualização">
+          @endif
         </div>
+
+        <div class="image-preview" id="image-preview">
+          @if($imagePath)
+          <img id="preview-image" src="{{ asset($imagePath) }}" alt="Imagem carregada">
+          @else
+          <img id="preview-image" src="img/upload.png" alt="Pré-visualização">
+          @endif
+        </div>
+
+        <div class="form-container" id="form-container">
+
+        </div>
+        <p class="usuario">{{ $nome }}</p>
+        <p class="cargo">{{ $cargo }}</p>
+
+        <img class="footer-img" src="img/footer-img1.png" alt="footer">
+
+        <!-- Tag Matricula que ficará sobre a imagem -->
+        <p class="matricula">Matrícula <span class="cod-matricula">{{ $matricula }}</span></p>
+      </div>
+
+      <!-- cracha2 -->
+      <div class="card-header">
+
+      </div>
+      <div class="card-body">
+        <img src="img/rodape.png" alt="Imagem do Cracha" class="centralizar-imagem">
+
+        <div class="drop-area1" id="drop-area">
+          <img id="preview-image" src="{{ $qrcodeUrl }}" alt="QR Code">
+        </div>
+
+        <div class="image-preview" id="image-preview">
+          <img id="preview-image" src="{{ $qrcodeUrl }}" alt="QR Code">
+        </div>
+
+        <div class="form-container" id="form-container">
+          <form method="POST" action="/upload" enctype="multipart/form-data">
+            @csrf
+            <input type="hidden" name="image_data" id="image_data">
+            <button type="submit" class="btn btn-primary">Enviar Imagem</button>
+          </form>
+        </div>
+
+        <img class="footer-img1" src="img/cracha-rodape-costas.png" alt="footer">
+      </div>
     </div>
+  </div>
 
 </body>
 
-<script>
-    const dropArea = document.getElementById("drop-area");
-    const dropText = document.getElementById("drop-text");
-    const previewImage = document.getElementById("preview-image");
-    const imagePreview = document.getElementById("image-preview");
-    const formContainer = document.getElementById("form-container");
-    const imageDataInput = document.getElementById("image_data");
-
-    // Impede o comportamento padrão de dragover para permitir o drop
-    dropArea.addEventListener("dragover", (e) => {
-        e.preventDefault();
-        dropArea.classList.add("dragover");
-    });
-
-    dropArea.addEventListener("dragleave", () => {
-        dropArea.classList.remove("dragover");
-    });
-
-    dropArea.addEventListener("drop", (e) => {
-        e.preventDefault();
-        dropArea.classList.remove("dragover");
-
-        const file = e.dataTransfer.files[0];
-        if (file && file.type.startsWith("image/")) {
-            showImagePreview(file);
-        } else {
-            alert("Por favor, arraste uma imagem válida.");
-        }
-    });
-
-    // Permitir seleção de arquivo ao clicar na área de arrastar
-    dropArea.addEventListener("click", () => {
-        const fileInput = document.createElement("input");
-        fileInput.type = "file";
-        fileInput.accept = "image/*";
-        fileInput.onchange = (e) => {
-            const file = e.target.files[0];
-            if (file && file.type.startsWith("image/")) {
-                showImagePreview(file);
-            } else {
-                alert("Por favor, selecione uma imagem válida.");
-            }
-        };
-        fileInput.click();
-    });
-
-    // Função para mostrar a pré-visualização da imagem
-    function showImagePreview(file) {
-        const reader = new FileReader();
-        reader.onload = () => {
-            // Substitui o texto da área de arrasto pela imagem
-            dropText.style.display = "none";
-            const imgElement = document.createElement("img");
-            imgElement.src = reader.result;
-            dropArea.innerHTML = ''; // Limpa o conteúdo da área
-            dropArea.appendChild(imgElement); // Adiciona a imagem à área
-
-            // Exibe a pré-visualização abaixo da área
-            previewImage.src = reader.result;
-            imagePreview.style.display = "block";
-
-            // Envia a imagem no FormData
-            imageDataInput.value = reader.result; // Armazena o Data URL da imagem no campo hidden
-
-            // Exibe o formulário
-            formContainer.style.display = "block";
-        };
-        reader.readAsDataURL(file);
-    }
-</script>
 
 @endsection
--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+<link
+    href="https://fonts.googleapis.com/css2?family=Danfo&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+    rel="stylesheet">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,18 +16,76 @@
             box-sizing: border-box;
         }
 
-        .cracha {
-            page-break-before: always; /* Garante que cada crachá fique em uma página separada */
+        .drop-area {
+            width: 40px;
+            height: 80px;
+            border: 2px solid black;
+            border-radius: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #1c56a8;
+            font-size: 5px;
+            cursor: pointer;
+            text-align: center;
+            position: relative;
+
+        }
+
+
+        .drop-area,
+        .image-preview {
+            margin-top: 15px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100px;
+            border: 2px solid #245ca8;
+            height: 120px;
+        }
+
+        .drop-area.dragover {
+            background-color: #f0f8f0;
+        }
+
+        .drop-area img {
+            max-width: 100%;
+            max-height: 100%;
+            border-radius: 8px;
+        }
+
+        .image-preview {
+            display: none;
+            margin-top: 70px;
+            text-align: center;
+
+        }
+
+        .form-container {
+            margin-top: 20px;
+            text-align: center;
+            display: none;
+        }
+
+        .centralizar-imagem {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 226px;
+        }
+
+        .card-container {
             width: 360px;
             margin: 20px auto;
             border: 1px solid #ddd;
             border-radius: 0px;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            overflow: hidden;
             background-color: transparent;
         }
 
         .card-header {
-            padding: 0;
+            padding: 0px;
             background-color: transparent;
             color: white;
             font-size: 14px;
@@ -41,43 +103,56 @@
 
         .card-body img {
             max-width: 100%;
-            border-radius: 0;
+            border-radius: 0px;
         }
 
         .usuario {
             color: #1c56a8;
-            font-weight: 500;
+            font-weight: 900;
             font-family: "inter", Courier, monospace;
             padding: 5px;
-            margin-bottom: 0;
-            font-size: 12px;
+            margin-bottom: 0px;
+            font-size: 15px;
         }
 
         .cargo {
             font-family: "inter", Courier, monospace;
-            margin-top: 0;
-            font-size: 10px;
+            margin-top: 0px;
+            font-size: 12px;
+            font-weight: 700;
+            color: gray;
         }
 
-        .matricula {
-            position: absolute;
-            top: 10px;
-            right: 90px;
-            font-size: 10px;
-            color: #ffffff;
-            background-color: transparent;
-            padding: 5px;
-            z-index: 10;
+        .usuario,
+        .cargo {
+            margin-bottom: 0px;
+
         }
 
         .footer-img {
-            width: 196px;
-            height: 36px;
-            margin-top: 30px;
+            width: 226px;
+            height: 40px;
+            margin-top: 20px;
         }
 
-        .qr-code {
-            margin-top: 20px;
+
+        .crachas-container {
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+            align-items: center;
+        }
+
+        /* Nova classe para aumentar a margem do preview da imagem */
+        .preview-image-second-cracha {
+            margin-top: 10px;
+            /* Ajuste conforme necessário */
+        }
+
+        .footer-img1 {
+            width: 226px;
+            height: 40px;
+            margin-top: 57px;
         }
 
         .titulo {
@@ -88,18 +163,69 @@
             margin-bottom: 2px;
         }
 
-        .footer-img1 {
-            width: 196px;
-            height: 36px;
-            margin-top: 60px;
+        .drop-area1 {
+            width: 90px;
+            height: 90px;
+            border: 2px solid #245ca8;
+            border-radius: 5px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #245ca8;
+            font-size: 5px;
+            cursor: pointer;
+            text-align: center;
+            position: relative;
+            margin-top: 50px;
+            margin-bottom: -6px;
+        }
+
+        .card-body {
+            position: relative;
+        }
+
+        .matricula {
+            position: absolute;
+            top: 27px;
+            right: 865px;
+            font-size: 12px;
+            color: #ffffff;
+            background-color: transparent;
+            padding: 5px;
+            z-index: 10;
+            margin-top: 312px;
+            margin-bottom: 600px;
+        }
+
+        .footer-img {
+            position: relative;
+            z-index: 1;
+            margin-top: 20px;
+        }
+
+        .cod-matricula {
+            font-weight: 700;
+        }
+
+        .qr-code {
+
+            margin-top: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 90px;
+            border: 2px solid #245ca8;
+            height: 90px;
+            border-radius: 8px;
         }
     </style>
 </head>
+
 <body>
     <!-- Crachá 1 -->
     <div class="cracha">
         <div class="card-body" style="position: relative;">
-            <img src="img/cracha-frente-barra1.png" alt="Imagem do Cracha" class="centralizar-imagem">
+            <img src="{{ asset('img/cracha-frente-barra1.png') }}" alt="Imagem do Cracha">
 
             <div class="drop-area">
                 <img id="preview-image" src="{{ asset($imagePath) }}" alt="Imagem carregada">
@@ -108,7 +234,7 @@
             <p class="usuario">{{ $nome }}</p>
             <p class="cargo">{{ $cargo }}</p>
 
-            <img class="footer-img" src="img/footer-img1.png" alt="footer">
+            <img class="footer-img" src="{{ asset('img/footer-img1.png') }}" alt="footer">
 
             <p class="matricula">Matrícula <span class="cod-matricula">{{ $matricula }}</span></p>
         </div>
@@ -117,16 +243,17 @@
     <!-- Crachá 2 -->
     <div class="cracha">
         <div class="card-body">
-            <img src="img/rodape.png" alt="Imagem do Cracha" class="centralizar-imagem">
-            <p class="titulo">Presença</p>
+            <img src="{{ asset('img/rodape.png') }}" alt="Imagem do Cracha">
+
 
             <div class="qr-code">
                 <img src="{{ $qrcodeUrl }}" alt="QR Code">
             </div>
 
-            <img class="footer-img1" src="img/cracha-rodape-costas.png" alt="footer">
+            <img class="footer-img1" src="{{ asset('img/cracha-rodape-costas.png') }}" alt="footer">
         </div>
     </div>
 
 </body>
+
 </html>
